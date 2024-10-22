c168995e34d1b46e63492c5d9beca375e2779f31
John Hodrien <J.H.Hodrien@leeds.ac.uk>
2024-10-22 13:52:29 +0100

    Update install.sh

--- src/novnc-tweaks/install.sh
+++ src/novnc-tweaks/install.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "The value of PROMPT is: ${PROMPT}"
+echo "The value of RESIZE is: ${RESIZE}"
 
 cd /usr/local/novnc/noVNC-1.2.0/app
 patch << EOB
@@ -17,4 +18,17 @@ patch << EOB
              autoconnect = true;
              UI.connect();
 EOB
-cd -
+
+patch << EOB
+--- ui.js.orig	2024-10-22 12:50:44.520322126 +0000
++++ ui.js	2024-10-22 12:50:32.816322227 +0000
+@@ -160,7 +160,7 @@
+         UI.initSetting('port', port);
+         UI.initSetting('encrypt', (window.location.protocol === "https:"));
+         UI.initSetting('view_clip', false);
+-        UI.initSetting('resize', 'none');
++        UI.initSetting('resize', 'remote');
+         UI.initSetting('quality', 6);
+         UI.initSetting('compression', 2);
+         UI.initSetting('shared', true);
+EOB
